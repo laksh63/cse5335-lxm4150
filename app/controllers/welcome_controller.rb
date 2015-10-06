236a3673e80f25f2db79176d89a295556fa8bfd1
lakshmi manjunatha <laksh63@gmail.com>
2015-10-06 16:52:20 -0500

    final commit

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,7 +6,11 @@ class WelcomeController < ApplicationController
 					{fname: 'Karthik', student: 'NO', City: 'Dallas', address: ' 17817 Coit Road, Dallas 75252, USA'},
 					{fname: 'Sharan', student: 'YES', City: 'Arlington', address: '1000 S Pecan St,Arlington, Tx 76010, USA'},
 					{fname: 'Rohit', student: 'YES', City:'Dallas', address: ' 415 S West street,Arlington, Tx 76010, USA'},
-					{fname: 'rahul', student: 'YES', City: 'Arlington', address: ' 415 S West street,Arlington, Tx 76010, USA'},
-					{fname: 'Akanksha', student: 'No', City: 'Arlington', address: ' 1002 Greek row drive,Arlington, Tx 76013, USA'}].to_json
+					{fname: 'Rahul', student: 'YES', City: 'Arlington', address: ' 415 S West street,Arlington, Tx 76010, USA'},
+					{fname: 'Shreyas', student: 'No', City: 'Arlington', address: ' 1000 N Collins Street,Arlington, Tx 76011, USA'},
+					{fname: 'Akanksha', student: 'No', City: 'Arlington', address: ' 3811 S Cooper Street, Arlington, Tx 76015, USA'},
+					{fname: 'Akshay', student: 'YES', City: 'Arlington', address: ' 1002 Greek row drive,Arlington, Tx 76013, USA'},
+					{fname: 'Neha', student: 'YES', City: 'Arlington', address: ' 3811 S Cooper Street, Arlington, Tx 76015, USA'},
+					{fname: 'Arun', student: 'YES', City: 'Arlington', address: ' 1000 N Collins Street,Arlington, Tx 76011, USA'}].to_json
   end
 end
